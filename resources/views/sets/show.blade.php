df4b143b07f38e437095afb67656b30cb244c4a1
Iwona Jaśtak <iwona.jastak1@gmail.com>
2018-04-21 22:37:26 +0200

    add categories, subcategories and sets

---
+++ resources/views/sets/show.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.default')
+
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2> Show Member</h2>
+            </div>
+
+        </div>
+    </div>
+
+@endsection
